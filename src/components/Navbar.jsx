032b5159484332bfe40cdcf68472e51a050f51b2
KhadijaBoosony <bossonykhadijae@gmail.com>
2026-02-19 21:41:30 +0000

    navbar dropdown added fixing the crowded elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,47 @@
-import { FaJsfiddle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { Sun, Moon } from 'lucide-react';
 import "./Navbar.css";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
-      const [isDarkMode, setIsDarkMode] = useState(true);
-      
-      // Toggle dark/light mode
-      useEffect(() => {
-        document.body.classList.toggle('light-mode', !isDarkMode);
-      }, [isDarkMode]);
-      
-    
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  // Toggle dark/light mode
+  useEffect(() => {
+    document.body.classList.toggle('light-mode', !isDarkMode);
+  }, [isDarkMode]);
+
   return (
     <nav className="navbar">
-       <div className="logo"  style={{ fontFamily: "'Courier New', Courier, monospace", fontWeight: 'bold', fontSize: '24px',   color: '#fff' }}
-        >Hi there!</div>
+      <div className="logo" style={{ fontFamily: "'Courier New', Courier, monospace", fontWeight: 'bold', fontSize: '24px', color: '#fff' }}>
+        Hi there!
+      </div>
 
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/education">Education</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/certifications">Certifications</Link>
+
+        {/* Dropdown for : Experience, Education, Skills, Certifications */}
+        <div className="dropdown">
+          <button className="dropbtn">About Me</button>
+          <div className="dropdown-content">
+            <Link to="/experience">Experience</Link>
+            <Link to="/education">Education</Link>
+            <Link to="/skills">Skills</Link>
+            <Link to="/certifications">Certifications</Link>
+          </div>
+        </div>
+
         <Link to="/contact">Contact</Link>
 
- {/* Theme Toggle Button */}
-          <button 
-            className="theme-toggle"
-            onClick={() => setIsDarkMode(!isDarkMode)}
-            aria-label="Toggle theme"
-          >
-            {isDarkMode ? <Sun size={20} /> : <Moon size={20} />}
-          </button>
-
-
+        <button 
+          className="theme-toggle"
+          onClick={() => setIsDarkMode(!isDarkMode)}
+          aria-label="Toggle theme"
+        >
+          {isDarkMode ? <Sun size={20} /> : <Moon size={20} />}
+        </button>
       </div>
-        
-          
-
-      
     </nav>
   );
 };
