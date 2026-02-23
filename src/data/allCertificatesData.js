b2cd60c3ebdf21193af1ebdfe2e14db9d756528f
KhadijaBoosony <bossonykhadijae@gmail.com>
2026-02-23 14:17:48 +0000

    created data folder

---
+++ src/data/allCertificatesData.js
@@ -0,0 +1,42 @@
+export const allCertificatesData = [
+  {
+    title: "Introduction à la programmation orientée objet (en C++)",
+    issuer: "École Polytechnique Fédérale de Lausanne",
+    date: "Jan 25, 2025",
+    verifyUrl: "https://coursera.org/verify/Y96650MDiD4Z",
+    skills: ["C++", "OOP", "Programming"],
+    color: "#0047AB"
+  },
+  {
+    title: "Successful Negotiation: Essential Strategies and Skills",
+    issuer: "University of Michigan",
+    date: "Jan 24, 2025",
+    verifyUrl: "https://coursera.org/verify/66DM8ST9LLW8",
+    skills: ["Negotiation", "Communication", "Business Strategy"],
+    color: "#FFCB05"
+  },
+  {
+    title: "Interactivity with JavaScript",
+    issuer: "University of Michigan",
+    date: "Jan 24, 2025",
+    verifyUrl: "https://coursera.org/verify/DJENODDKNBXL",
+    skills: ["JavaScript", "DOM", "Web Development"],
+    color: "#F7DF1E"
+  },
+  {
+    title: "Software Engineering: Software Design and Project Management",
+    issuer: "HKUST",
+    date: "2025",
+    verifyUrl: "https://coursera.org/verify/DY3PVODCXN4Z",
+    skills: ["Software Design", "Project Management", "Agile"],
+    color: "#003366"
+  },
+  {
+    title: "Using Python to Access Web Data",
+    issuer: "University of Michigan",
+    date: "Jun 1, 2025",
+    verifyUrl: "https://coursera.org/verify/2OZ|KTYLBMB9",
+    skills: ["Python", "Web Scraping", "APIs", "JSON"],
+    color: "#306998"
+  }
+];
---
+++ src/data/websiteDemosData.js
@@ -0,0 +1,14 @@
+export const websiteDemosData = [
+  { 
+    title: 'forum', 
+    image: 'forumPic.jpeg' 
+  },
+  { 
+    title: 'leoni2',
+    image: 'LeoniPic2.jpg'
+  },
+  { 
+    title: 'forum2', 
+    image: 'forumPic2.jpeg' 
+  }
+];
